19d7be8d01ba3222212a2b6a81a6a4dcdcad4fd6
Mei Zhou <meiyo@Meis-MacBook-Air.local>
2024-03-08 00:33:01 -0800

    css changes

--- src/components/home.js
+++ src/components/home.js
@@ -37,67 +37,69 @@ export function Home ({symptoms , onDeleteSymptom}) {
         }, [location.state]);
 
     return (
-        <div>
-            <main id="main_page">
-                    <div className="container">
-                        <div className="tracker">
-                            <section className="head">
-                                <h2>Symptoms</h2>
-                                <div className="filter">
-                                    <Link to="/form" className="symptom_add">
-                                        <img src={addImg} alt="Add Icon"/>
-                                    </Link>
-                                </div>
-                            </section>
-                            <ul>
-                                {symptoms.map(symptom => (
-                                    <section className="symptom" key={symptom.id}>
-                                        <li>
-                                            <Link to={`/editForm/${symptom.id}`} className="symptom_link">{symptom.text}</Link> 
-                                            <div className="symptom-button">
-                                                <Link to={`/editForm/${symptom.id}`} className="symptom_form">
-                                                    <img src={editImg} alt="Edit" />
-                                                </Link>
-                                                <button className="trash-icon" onClick={() => onDeleteSymptom(symptom.id)}>
-                                                    <img src={trashImg} alt="Trash Icon" />
-                                                </button>
-                                            </div>
-                                        </li>
-                                    </section>
-                                ))}
-                            </ul>
-
-                        </div>
-
-
-                        <div className="quick-links">
-                            <div className="link-box">
+        <div id ="wrapper">
+            <div id="contents">
+                <main id="main_page">
+                        <div className="container">
+                            <div className="tracker">
                                 <section className="head">
-                                    <h2>Quick Links</h2>
+                                    <h2>Symptoms</h2>
+                                    <div className="filter">
+                                        <Link to="/form" className="symptom_add">
+                                            <img src={addImg} alt="Add Icon"/>
+                                        </Link>
+                                    </div>
                                 </section>
-
-                                <div className="link">
-                                    <a href="https://www.mychart.org/" target="_blank"><img src={mycharImg} alt="my chart" />My Chart</a>
-                                </div>
-                                <div className="link">
-                                    <a href="https://wellbeing.uw.edu/mental-health/mental-health-resources/" target="_blank"><img src={uwMHImg} alt="uw resources" />UW Mental Health Resources</a>
-                                </div>
-                                <div className="link">
-                                    <a href="https://www.webmd.com/" target="_blank"><img src={webMDImg} alt="webmd" />WebMD</a>
-                                </div>
-                                <div className="link">
-                                    <a href="https://seattlecenter.org/skcclinic/" target="_blank"><img src={seaHealthImg} alt="Seattle Center Foundation" />Seattle Center Foundation</a>
-                                </div>
+                                <ul>
+                                    {symptoms.map(symptom => (
+                                        <section className="symptom" key={symptom.id}>
+                                            <li>
+                                                <Link to={`/editForm/${symptom.id}`} className="symptom_link">{symptom.text}</Link> 
+                                                <div className="symptom-button">
+                                                    <Link to={`/editForm/${symptom.id}`} className="symptom_form">
+                                                        <img src={editImg} alt="Edit" />
+                                                    </Link>
+                                                    <button className="trash-icon" onClick={() => onDeleteSymptom(symptom.id)}>
+                                                        <img src={trashImg} alt="Trash Icon" />
+                                                    </button>
+                                                </div>
+                                            </li>
+                                        </section>
+                                    ))}
+                                </ul>
 
                             </div>
 
+
+                            <div className="quick-links">
+                                <div className="link-box">
+                                    <section className="head">
+                                        <h2>Quick Links</h2>
+                                    </section>
+
+                                    <div className="link">
+                                        <a href="https://www.mychart.org/" target="_blank"><img src={mycharImg} alt="my chart" />My Chart</a>
+                                    </div>
+                                    <div className="link">
+                                        <a href="https://wellbeing.uw.edu/mental-health/mental-health-resources/" target="_blank"><img src={uwMHImg} alt="uw resources" />UW Mental Health Resources</a>
+                                    </div>
+                                    <div className="link">
+                                        <a href="https://www.webmd.com/" target="_blank"><img src={webMDImg} alt="webmd" />WebMD</a>
+                                    </div>
+                                    <div className="link">
+                                        <a href="https://seattlecenter.org/skcclinic/" target="_blank"><img src={seaHealthImg} alt="Seattle Center Foundation" />Seattle Center Foundation</a>
+                                    </div>
+
+                                </div>
+
+                            </div>
                         </div>
-                    </div>
-            </main>
-            <footer className="footer">
-                <p>This page was created by our Info 340 team</p>
-                <p>&copy; 2024 SymptoTrack.</p>
-            </footer>
+                </main>
+            </div>
+        <footer className="footer">
+            <p>This page was created by our Info 340 team</p>
+            <p>&copy; 2024 SymptoTrack.</p>
+        </footer>
         </div>
         )
     };
